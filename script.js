95e5a76264db1c039e6fbe7e351e4fb86c514a47
Matheus da Cunha da Fonseca <matheus.cunha.fonseca50@gmail.com>
2023-08-03 11:06:12 -0300

    Verificador se o navegador está em dark mode

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const ball = document.querySelector(".ball");
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (isDarkMode) {
+  document.body.classList.add("dark");
+}
 
 ball.addEventListener("click", () => {
   document.body.classList.toggle("dark");
